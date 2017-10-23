7d4e655bf75ea193068fa849965150fc9e3b5580
Matheus Vinhas <matheusfvinhas@gmail.com>
2017-10-23 21:17:30 -0200

    add index view edital e evento

--- spec/controllers/edital_controller_spec.rb
+++ spec/controllers/edital_controller_spec.rb
@@ -1,33 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe EditaisController, type: :controller do
-
-    describe "GET show" do
-        it "get @edital" do      
-          get :show
-          expect(response).to be_success
-        end      
-      end  
       
-      describe "GET new" do
-        it "get new @edital" do      
-          get :new
-          expect(response).to be_success
-        end      
-      end 
+  describe "GET new" do
+    it "get new @edital" do      
+      get :new
+      expect(response).to be_success
+    end      
+  end 
 
-      describe "GET index" do
-        it "get @editais" do      
-          get :index
-          expect(response).to be_success
-        end      
-      end 
-
-      describe "GET edit" do
-        it "get @edital" do      
-          get :edit
-          expect(response).to be_success
-        end      
-      end 
-
+  describe "GET index" do
+    it "get @editais" do      
+      get :index
+      expect(response).to be_success
+    end      
+  end  
+  
 end
