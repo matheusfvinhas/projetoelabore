8659f39bdd2f16006c59bd48aa0855fcc49eeac3
Awesome Code <team@xinminlabs.com>
2018-01-02 11:43:55 +0000

    Auto corrected by following EmptyLine

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -27,7 +27,6 @@ class NoticesController < ApplicationController
     end
 
     def edit
-
     end
 
     def update
@@ -50,6 +49,7 @@ class NoticesController < ApplicationController
     end
 
     private
+
         def set_notice
             @notice = Notice.find(params[:id])
         end
