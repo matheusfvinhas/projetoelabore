b72bb94d7935c7af259bb28eb36b7435589be0cb
Awesome Code <team@xinminlabs.com>
2018-01-02 12:21:42 +0000

    Auto corrected by following Layout/TrailingWhitespace

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,7 +4,7 @@ class Event < ApplicationRecord
   mount_uploaders :images, ImageUploader
 
     belongs_to :user
-    
+
     validates :title, presence: true, length: { maximum: 100 }
     validates :description, presence: true, length: { maximum: 255 }
     validates :local, presence: true, length: { maximum: 100 }
