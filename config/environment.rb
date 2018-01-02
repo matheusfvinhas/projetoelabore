3cb4a63213ee9bfd42475893c807b3a62d7eddb1
Awesome Code <team@xinminlabs.com>
2018-01-02 11:46:33 +0000

    Auto corrected by following Style/MutableConstant

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,6 @@ Rails.application.initialize!
 
 
 Time::DATE_FORMATS[:event_time] = "%H:%M"
-PRIMARY_BUTTONS_COLOR = "yellow darken-3"
-BACK_BUTTONS_COLOR = "grey"
+PRIMARY_BUTTONS_COLOR = "yellow darken-3".freeze
+BACK_BUTTONS_COLOR = "grey".freeze
 
