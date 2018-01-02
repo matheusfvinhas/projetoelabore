a42d53a750688d4d6d6dae937bdee84b45b28efd
Awesome Code <team@xinminlabs.com>
2018-01-02 12:28:15 +0000

    Auto corrected by following Style/HashSyntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations' }
   post 'create_user' => 'users#create', as: :create_user   
   get 'register' => 'users#new', as: :register_user
   get 'usuarios' => 'users#index', as: :show_all_users   
