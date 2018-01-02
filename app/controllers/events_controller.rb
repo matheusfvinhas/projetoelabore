82976d6392a8fe2162c532f868368a3322cfb853
Awesome Code <team@xinminlabs.com>
2018-01-02 12:51:45 +0000

    Auto corrected by following GeneralLayout

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class EventsController < ApplicationController
-    before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index]
     before_action :set_event, only: %i[show edit update destroy]
 
     def new
