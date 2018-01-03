8a5b16034406d1a08d6c53735a141063e5b38c73
Matheus Vinhas <matheusfvinhas@gmail.com>
2018-01-02 23:15:05 -0200

    fixed password confirmation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,25 +2,29 @@
 
 class RegistrationsController < Devise::RegistrationsController
   def update
-    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
-    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+    if validate_password
+      self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+      prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
 
-    resource.new_user = false
+      resource.new_user = false
 
-    resource_updated = update_resource(resource, account_update_params)
-    yield resource if block_given?
-    if resource_updated
-      if is_flashing_format?
-        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
-          :update_needs_confirmation : :updated
-        set_flash_message :notice, flash_key
+      resource_updated = update_resource(resource, account_update_params)
+      yield resource if block_given?
+      if resource_updated
+        if is_flashing_format?
+          flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+            :update_needs_confirmation : :updated
+          set_flash_message :notice, flash_key
+        end
+        bypass_sign_in resource, scope: resource_name
+        respond_with resource, location: after_update_path_for(resource)
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        respond_with resource
       end
-      bypass_sign_in resource, scope: resource_name
-      respond_with resource, location: after_update_path_for(resource)
     else
-      clean_up_passwords resource
-      set_minimum_password_length
-      respond_with resource
+      render 'devise/registrations/edit'
     end
   end
 
@@ -29,5 +33,15 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:username, :name, :responsible, :telephone, :mini_cv, :email, :password, :current_password, :password_confirmation)
   end
-  
+
+  def validate_password
+    if params[:user][:password] != params[:user][:password_confirmation]
+      flash[:alert] = 'Senha e Confirmação de Senha devem ser iguais.'
+      false
+    else
+      flash[:alert] = ''
+      true
+    end
+  end
+
 end
