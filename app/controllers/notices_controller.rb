8ba0cb3006f8f3b4c38c454d0f12b17390cae8ac
Matheus Vinhas <matheusfvinhas@gmail.com>
2018-01-15 22:51:33 -0200

    changing identation

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -2,65 +2,65 @@
 
 class NoticesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
-    before_action :set_notice, only: %i[show edit update destroy]
-    before_action :config_link, only: %i[create update]
+  before_action :set_notice, only: %i[show edit update destroy]
+  before_action :config_link, only: %i[create update]
 
-    def new
-      @notice = Notice.new
-    end
+  def new
+    @notice = Notice.new
+  end
 
-    def index
-      @notices = Notice.all.order(created_at: :desc).page(params[:page]).per(6)
-    end
+  def index
+    @notices = Notice.all.order(created_at: :desc).page(params[:page]).per(6)
+  end
 
-    def show; end
+  def show; end
 
-    def create
-      @notice = Notice.new(notice_params)
+  def create
+    @notice = Notice.new(notice_params)
 
-      if @notice.save
-        flash[:notice] = 'Edital salvo com sucesso.'
-          redirect_to notices_path
-      else
-        flash[:alert] = 'Erro ao salvar edital.'
-          render :new
-      end
-    end
-
-    def edit; end
-
-    def update
-      if @notice.update(notice_params)
-        flash[:notice] = 'Edital atualizado com sucesso.'
+    if @notice.save
+      flash[:notice] = 'Edital salvo com sucesso.'
         redirect_to notices_path
-      else
-        flash[:alert] = 'Erro ao atualizar edital.'
-        render :edit
-      end
+    else
+      flash[:alert] = 'Erro ao salvar edital.'
+        render :new
     end
+  end
 
-    def destroy
-      if @notice.destroy
-        flash[:notice] = 'Edital deletado com sucesso.'
-      else
-        flash[:alert] = 'Erro ao deletar edital.'
-      end
+  def edit; end
+
+  def update
+    if @notice.update(notice_params)
+      flash[:notice] = 'Edital atualizado com sucesso.'
       redirect_to notices_path
+    else
+      flash[:alert] = 'Erro ao atualizar edital.'
+      render :edit
     end
+  end
 
-  private
-
-    def set_notice
-      @notice = Notice.find(params[:id])
+  def destroy
+    if @notice.destroy
+      flash[:notice] = 'Edital deletado com sucesso.'
+    else
+      flash[:alert] = 'Erro ao deletar edital.'
     end
+    redirect_to notices_path
+  end
 
-    def notice_params
-      params.require(:notice).permit(:title, :description, :document, :link).merge(user_id: current_user.id)
-    end
+private
 
-    def config_link      
-      if !params[:notice][:link].start_with?('http') && !params[:notice][:link].blank?
-        params[:notice][:link] = "http://#{params[:notice][:link]}"
-      end
+  def set_notice
+    @notice = Notice.find(params[:id])
+  end
+
+  def notice_params
+    params.require(:notice).permit(:title, :description, :document, :link).merge(user_id: current_user.id)
+  end
+
+  def config_link      
+    if !params[:notice][:link].start_with?('http') && !params[:notice][:link].blank?
+      params[:notice][:link] = "http://#{params[:notice][:link]}"
     end
+  end
 end
--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -3,66 +3,66 @@
 class PartnersController < ApplicationController
   before_action :authenticate_user!, only: %i[index show confirm_partner_apply]
 
-    def new
-      @partner = Partner.new
-    end
+  def new
+    @partner = Partner.new
+  end
 
-    def index
-      @partners = Partner.all.order(created_at: :desc).page(params[:page]).per(10)
-    end
+  def index
+    @partners = Partner.all.order(created_at: :desc).page(params[:page]).per(10)
+  end
 
-    def show
-      @partner = Partner.find(params[:id])
-    end
+  def show
+    @partner = Partner.find(params[:id])
+  end
 
-    def confirm_partner_apply
-      @partner = Partner.find(params[:id])
-      @user = create_user(@partner)        
+  def confirm_partner_apply
+    @partner = Partner.find(params[:id])
+    @user = create_user(@partner)        
+  
+    if @user.save
+      flash[:notice] = 'Parceria confirmada com sucesso.'
+        confirm_partner            
+        UserMailer.welcome(@user, @user.password).deliver_later            
+    else
+      flash[:alert] = 'Erro ao confirmar parceria.'          
+    end
     
-      if @user.save
-        flash[:notice] = 'Parceria confirmada com sucesso.'
-          confirm_partner            
-          UserMailer.welcome(@user, @user.password).deliver_later            
-      else
-        flash[:alert] = 'Erro ao confirmar parceria.'          
-      end
+    redirect_to partners_path
+  end
+
+  def send_partner_apply
+    @partner = Partner.new(partner_params)
+    @partner.confirmed = false      
       
-      redirect_to partners_path
+    if @partner.save
+      flash[:notice] = 'Sua solicitação foi enviada com sucesso.'
+      PartnersMailer.new_partner(@partner).deliver_later
+      redirect_to root_path
+    else
+      flash[:alert] = 'Erro ao enviar solicitação.'  
+      render :new        
     end
+  end
 
-    def send_partner_apply
-      @partner = Partner.new(partner_params)
-      @partner.confirmed = false      
-        
-      if @partner.save
-        flash[:notice] = 'Sua solicitação foi enviada com sucesso.'
-        PartnersMailer.new_partner(@partner).deliver_later
-        redirect_to root_path
-      else
-        flash[:alert] = 'Erro ao enviar solicitação.'  
-        render :new        
-      end
-    end
+private
 
-  private
+  def partner_params
+    params.require(:partner).permit(:name, :responsible, :email, :about, :telephone)
+  end
 
-    def partner_params
-      params.require(:partner).permit(:name, :responsible, :email, :about, :telephone)
-    end
+  def confirm_partner
+    @partner.confirmed = true
+    @partner.save!
+  end
 
-    def confirm_partner
-      @partner.confirmed = true
-      @partner.save!
-    end
-
-    def create_user(partner)
-      user = User.new
-      user.name = partner.name
-      user.responsible = partner.responsible
-      user.email = partner.email
-      user.password = Devise.friendly_token.first(8)
-      user.kind = :student
-      user.telephone = partner.telephone
-      user
-    end
+  def create_user(partner)
+    user = User.new
+    user.name = partner.name
+    user.responsible = partner.responsible
+    user.email = partner.email
+    user.password = Devise.friendly_token.first(8)
+    user.kind = :student
+    user.telephone = partner.telephone
+    user
+  end
 end
