603f61968eb80bb44a91cecab79360995a737936
Matheus Vinhas <matheusfvinhas@gmail.com>
2018-02-18 13:00:38 -0300

    send question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,19 @@
 class QuestionsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_enrollment
 
   def ask_question
-    @enrollment = Enrollment.find(params[:enrollment_id])
     @grade = Grade.find(params[:grade_id])
   end
 
   def send_question
-    redirect_to enrollment_path(params[:enrollment_id])
+    QuestionsMailer.send_question(params[:grade_title], params[:email_teacher], params[:email_student], @enrollment.user.name, params[:question], @enrollment.course.title).deliver_later
+    flash[:notice] = 'Pergunta enviada com sucesso.'
+    redirect_to enrollment_path(@enrollment)
   end
+
+  private
+    def set_enrollment
+      @enrollment = Enrollment.find(params[:enrollment_id])
+    end
 end
--- app/mailers/questions_mailer.rb
+++ app/mailers/questions_mailer.rb
@@ -1,2 +1,10 @@
 class QuestionsMailer < ApplicationMailer
+  def send_question(grade_title, email_teacher, email_student, name_student, question, course_title)
+    @grade_title = grade_title
+    @question = question
+    @course_title = course_title
+    @name_student = name_student
+    @email_student = email_student
+    mail to: email_teacher, from: email_student, subject: "Pergunta Curso Elabore: #{course_title}"
+  end
 end
