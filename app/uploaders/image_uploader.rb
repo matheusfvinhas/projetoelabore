cecb4fbf1471202dba0aad18eb0d105817c5bb4e
Matheus Vinhas <matheusfvinhas@gmail.com>
2018-03-06 23:35:34 -0300

    bug fix

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 class ImageUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+    include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
