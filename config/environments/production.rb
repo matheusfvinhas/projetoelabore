60daf586cb68cc4c7f99eaeaf5f1cfd5b206e638
Matheus Vinhas <matheusfvinhas@gmail.com>
2018-04-17 23:20:26 -0300

    changed host mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'projetoelabore.com.br' }
-  Rails.application.routes.default_url_options[:host] = 'projetoelabore.com.br'
+  config.action_mailer.default_url_options = { host: ENV['HOST_MAILER'] }
+  Rails.application.routes.default_url_options[:host] = ENV['HOST_MAILER']
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
