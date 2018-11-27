04a56329dc79e65075b3a71c27063d42f26ed2e7
Matheus Vinhas <matheusfvinhas@gmail.com>
2018-11-27 18:09:30 -0200

    circle

--- spec/models/notice_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe Notice, type: :model do
-
-
-end
--- spec/models/tracking_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe Tracking, type: :model do
-
-end
