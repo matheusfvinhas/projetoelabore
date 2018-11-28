825b0040e9cd6c96cd8f2c9ceedf81ec16f29a15
Matheus Vinhas <matheusfvinhas@gmail.com>
2018-11-28 11:25:13 -0200

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 eval "$(ssh-agent -s)"
+openssl aes-256-cbc -K $encrypted_700619d89018_key -iv $encrypted_700619d89018_iv -in .travis/deploy.key.enc -out .travis/deploy.key -d
 chmod 600 .travis/deploy.key
 ssh-add .travis/deploy.key
 ssh-keyscan 68.183.139.87 >> ~/.ssh/known_hosts
