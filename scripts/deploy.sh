908e5fe3c2822a6db69e4fa2785a17dd2a8720e8
Matheus Vinhas <matheusfvinhas@gmail.com>
2018-11-28 11:13:29 -0200

    deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 eval "$(ssh-agent -s)"
-chmod 600 .travis/deploy.key
-ssh-add .travis/deploy.key
+chmod 600 ../.travis/deploy.key
+ssh-add ../.travis/deploy.key
 ssh-keyscan 68.183.139.87 >> ~/.ssh/known_hosts
 git remote add deploy dokku@68.183.139.87:elabore
 git config --global push.default simple
